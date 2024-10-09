0247083d764e656cb6282b6a54dbd01f96d5a9c6
Emes13 <moshe@moshe.us>
2024-10-09 17:59:56 -0400

    added function to split the string once instead of repeating that inside each function

--- simplelist.py
+++ simplelist.py
@@ -1,10 +1,21 @@
 #Little coding practice
 
+# To do:
+# 1. make search not case sensitive
+# 2. figure out how to avoid globals by returning list from function instead
+
 activesession = True
 user_defined_data = []
 
-def is_command(raw_text):
-    if raw_text[:3] == "]\ ":
+'''
+I want to wrap the user input in a function that will return a list of the terms, 
+rather than splitting the string on the fly every time
+'''
+def get_text():
+	return input("=> ").split(" ")
+
+def is_command(user_input_list):
+    if user_input_list[0] == "]": #use of \ was causing escape problems
         return True
     return False
     
@@ -12,12 +23,12 @@ def process_command(command):
     global activesession
     global user_defined_data
   
-    if command[3:] == "quit":
+    if command[1] == "quit":
         activesession = False
         return
     
-    if command[3:9] == "search":
-        search_term = command.split(" ")[2]
+    elif command[1] == "search": #elif for readability even tho prev if returns
+        search_term = command[2]
         term_exists = False # Does not exist until proven otherwise.
         for i in user_defined_data:
             if search_term in i:
@@ -27,21 +38,26 @@ def process_command(command):
         if not term_exists: print("Your search term was not found.")
         return
             
-    if command[3:7] == "list":
+    elif command[1] == "list":
         for i in user_defined_data:
             print(i,)
         return
+		
+    else:
+	    print("Invalid command.")
+	    return
 
 def store_data():
     global user_defined_data
-    user_input = input("=> ")
+    user_input_list = get_text()
     
-    if is_command(user_input):
-        process_command(user_input)
+    if is_command(user_input_list):
+        process_command(user_input_list)
     else:    
-        user_defined_data.append(user_input.split(" "))
+		#each separate word is listed separately
+        user_defined_data.extend(user_input_list) 
         
     return
     
-while activesession == True:
+while activesession:
     store_data()
