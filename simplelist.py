1f303a5e3f66aa32eaa189a2ee87247f985e059f
Emes13 <moshe@moshe.us>
2024-10-10 10:39:23 -0400

    removed need for global activesession

--- simplelist.py
+++ simplelist.py
@@ -1,8 +1,8 @@
 #Little coding practice
-
+# removed need for global activesession
 # To do:
 # 1. make search not case sensitive
-# 2. figure out how to avoid globals by returning list from function instead
+# 2. figure out how to avoid global by returning list from function instead
 
 activesession = True
 user_defined_data = []
@@ -19,13 +19,12 @@ def is_command(user_input_list):
         return True
     return False
     
-def process_command(command):
-    global activesession
+def process_command(command, activesession):
     global user_defined_data
   
     if command[1] == "quit":
         activesession = False
-        return
+        return activesession
     
     elif command[1] == "search": #elif for readability even tho prev if returns
         search_term = command[2]
@@ -36,28 +35,28 @@ def process_command(command):
                 print(i)
             
         if not term_exists: print("Your search term was not found.")
-        return
+        return activesession
             
     elif command[1] == "list":
         for i in user_defined_data:
             print(i,)
-        return
+        return activesession
 		
     else:
 	    print("Invalid command.")
-	    return
+	    return activesession
 
-def store_data():
+def store_data(activesession):
     global user_defined_data
     user_input_list = get_text()
     
     if is_command(user_input_list):
-        process_command(user_input_list)
+        activesession = process_command(user_input_list, activesession)
     else:    
 		#each separate word is listed separately
         user_defined_data.extend(user_input_list) 
         
-    return
+    return activesession
     
 while activesession:
-    store_data()
+    activesession = store_data(activesession)
