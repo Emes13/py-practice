6e4402854cb88beff89925759162eeabfae17306
Emes13 <moshe@moshe.us>
2024-11-14 00:48:55 -0500

    Added menu and custom alphabet functionality

--- gem.py
+++ gem.py
@@ -1,13 +1,25 @@
 from math import floor
+from os import system
+system('cls')
 
-gematriot = []
-alphabet_h = "אבגדהוזחטיכלמנסעפצקרשת"
-## 1) added method to convert end letters to non-end forms
-## 2) added reversed (correct) display of Hebrew using [::-1] string slicer
-alphabet_eng = "abcdefghijklmnopqrstuvwxyz"
-
+def menu(): # returns chosen alphabet for gematria encoding, and an identifier int
+    alphabets = {"English": "abcdefghijklmnopqrstuvwxyz", "Hebrew": "אבגדהוזחטיכלמנסעפצקרשת"}
+    while True: #invalid input just loops back to the question
+        menu_choice = input("Type 1 for English, 2 for Hebrew, or 3 to define your own alphabet: ")
+        if menu_choice == "1":
+            return alphabets["English"], 1
+        elif menu_choice == "2":
+            return alphabets["Hebrew"], 2
+        elif menu_choice == "3":
+            return input("Type your alphabet (do not use spaces): "), 3
+        else:
+            continue
+              
 def gem(x):
     if x > 0:
+        # An equation I came up with. It uses a starting index of 1 (sorry). As with classic gematria,
+        # every 10th number raises the sequence to the next power of 10.
+        # e.g. 1, 2, 3, ..., 9, 10, 20, 30, ..., 90, 100, 200, 300, ..., 900, 1000, ...
         gematria = (10**(floor((x-1)/9))) * (x - (9*floor((x-1)/9)))
     else:
         gematria = 0
@@ -41,25 +53,28 @@ def normalize(original, strip_space=False, verbose=False):
                 case _:
                     normalized_Hstring += char
         if original != normalized_Hstring and verbose == True:
-            print("One or more final forms were automatically replaced with non-final forms.")
-        return normalized_Hstring  
+            # warning in green text (tested only in PowerShell terminal)
+            print("\033[92mOne or more final forms were automatically replaced with non-final forms.\033[0m")
+        return normalized_Hstring
 
 def main():
-    english_switch = input("For English mode, type 'e' and Enter. Otherwise press Enter now: ").lower()
-    if english_switch == "e":
-        encoded_alpha = encode_alphabet(alphabet_eng) #dict
-        user_input = input("Enter the text to find the gematria: ").lower()
-    else:
-        encoded_alpha = encode_alphabet(alphabet_h)
-        user_input = normalize(input("Enter the text to find the gematria: "), strip_space=False, verbose=True)[::-1] #reverse for LTR display
-
+    gematriot = []
+    chosen_alphabet, identifier = menu()
+    encoded_alpha = encode_alphabet(chosen_alphabet) # dict
+    
+    # If alphabet is Hebrew, .lower() method has no effect; if it's English, normalize() has no effect (unlesss strip_space=True).
+    # Edge case: someone tries to define a custom alphabet including Hebrew final forms. It won't work.
+    user_input = normalize(input("Enter the text to find the gematria: ").lower(), strip_space=False, verbose=True)
+    
     for char in user_input:
         if char != " ": # tbd: implement better error checking
             gematriot.append(gem(encoded_alpha[char]))
     
     print(f"Input: {user_input}")
+    if identifier == 2: print(f"Standardized input: {user_input[::-1]}")
     print(f"The gematria of the entire text totals: {sum(gematriot)}")
-    print("Here are the individual values of each character:\n", gematriot)
+    print("Here are the individual values of each character:\n", gematriot,"\n")
     
-# Begin program
-main()
+# Begin program (looped)
+while True:
+    main()
